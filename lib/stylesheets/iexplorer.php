16290ec4f79a163505f4823d3ea7bb9064484553
Chuigda Whitegive <icey@icey.tech>
2025-05-21 04:32:49 +0800

    Okay，就这样吧

--- lib/stylesheets/iexplorer.php
+++ lib/stylesheets/iexplorer.php
@@ -1,16 +1,16 @@
-<!-- IE5 to IE9 support -->
+<!-- IE5 to IE9 support. There'll be an ugly horizontal scroll bar on IE6/7, quite unfortunate -->
 <!--[if gte IE 5]>
 <style type="text/css">
-body { overflow: auto; }
+body { box-sizing: border-box; padding: 0.5em; overflow: auto; }
 .tree-view { display: none; }
 .passage { margin: 1em; border: 1px solid black; }
 </style>
 <![endif]-->
 
-<!-- IE10/11 support -->
+<!-- IE10/11 support. Using @media since IE10/11 does not support if condition any more. -->
 <style>
 @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
-    body { overflow: auto; }
+    body { box-sizing: border-box; padding: 0.5em; overflow: auto; }
     .tree-view { display: none; }
     .passage { margin: 1em; border: 1px solid black; }
 }
